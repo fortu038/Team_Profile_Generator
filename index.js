743de82d1dea11b3973e49d9cd31ab771b4ce5cd
fortu038 <fortu038@umn.edu>
2022-10-13 13:00:35 -0500

    added HTML file build function to index.js

--- index.js
+++ index.js
@@ -45,7 +45,8 @@ const askAboutEngineer = async () => {
             } else if(answers.addMember === "Add intern") {
                 askAboutIntern();
             } else {
-                console.log(teamArray);
+                // console.log(teamArray);
+                buildHTML();
             }
         })
 }
@@ -88,7 +89,8 @@ const askAboutIntern = async () => {
             } else if(answers.addMember === "Add intern") {
                 askAboutIntern();
             } else {
-                console.log(teamArray);
+                // console.log(teamArray);
+                buildHTML();
             }
         })
 }
@@ -130,10 +132,71 @@ inquirer
         } else if(answers.addMember === "Add intern") {
             askAboutIntern();
         } else {
-            console.log(teamArray);
+            // console.log(teamArray);
+            buildHTML();
         }
     })
 
 function buildHTML() {
+    let teamCards = ""
+    for(let i = 0; i < teamArray.length; i++) {
+        let role = teamArray[i].getRole();
+        teamCards = teamCards.concat(`
+            <div class="col-4 mb-3">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body bg-primary text-white">
+                        <h5 class="card-title">${role}</h5>
+                        <h7 class="card-title">${teamArray[i].getName()}</h7>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">ID: ${teamArray[i].getId()}</li>
+                        <li class="list-group-item">Email: 
+                            <a href="mailto:${teamArray[i].getEmail()}">${teamArray[i].getEmail()}</a>
+                        </li>`);
+    
+        if(role === "Manager") {
+            teamCards = teamCards.concat(`
+                        <li class="list-group-item">Office Number: ${teamArray[i].getOfficeNumber()}</li>
+                    </ul>
+                </div>
+            </div>`);
+        } else if(role === "Engineer") {
+            teamCards = teamCards.concat(`
+                        <li class="list-group-item">Github: 
+                            <a href="https://github.com/${teamArray[i].getGithub()}">${teamArray[i].getGithub()}</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>`);
+        } else if(role === "Intern") {
+            teamCards = teamCards.concat(`
+                        <li class="list-group-item">School: ${teamArray[i].getSchool()}</li>
+                    </ul>
+                </div>
+            </div>`);
+        }
+}
+    fs.writeFile("./dist/team-profile.html", 
+    `<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link
+    rel="stylesheet"
+    href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    />
+    <title>Team Profile</title>
+</head>
+<body>
+    <header class="bg-danger text-white text-center">
+        <h1>My Team</h1>
+    </header>
 
+    <section class="container">
+        <div class="row d-flex justify-content-center">${teamCards}
+        </div>
+    </section>
+</body>
+</html>`, 
+    (err) => err ? console.error(err) : console.log('Success!'))
 }
